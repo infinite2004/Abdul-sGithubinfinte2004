79b6832a28e33c45431dbd4ee6f03d8f3b8da930
CodeNext <codenext@Admins-MBP-9.lan>
2019-02-05 14:27:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ function changeColors(id, colour){
 
 	document.getElementById(id).style.color = colour;
 
+  function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 }
